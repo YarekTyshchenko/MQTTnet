914abe4425f93ab62a17bfe0850e0b47c7946aed
Yarek Tyshchenko <yarek@harksys.com>
2022-12-13 12:26:47 +0000

    Add null checks to tenanting code

--- Source/MQTTnet/Server/Internal/Tenanting.cs
+++ Source/MQTTnet/Server/Internal/Tenanting.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace MQTTnet.Server
 {
-    
     public class Tenanting
     {
         /// <summary>
@@ -14,12 +13,19 @@ namespace MQTTnet.Server
         /// <returns></returns>
         public static bool CanSessionReceiveMessage(MqttApplicationMessage applicationMessage, MqttSession session)
         {
+            ArgumentNullException.ThrowIfNull(session, nameof(session));
+            ArgumentNullException.ThrowIfNull(session.Items, nameof(session.Items));
+
             if (!session.Items.Contains("OrganizationId"))
             {
                 throw new Exception("OrganizationId is missing from session so cannot send it messages");
             }
 
             var sessionOrgId = (string)session.Items["OrganizationId"];
+
+            ArgumentNullException.ThrowIfNull(applicationMessage, nameof(applicationMessage));
+            ArgumentNullException.ThrowIfNull(applicationMessage.UserProperties, nameof(applicationMessage.UserProperties));
+
             foreach (var userProperty in applicationMessage.UserProperties)
             {
                 if (userProperty.Name == "OrganizationId")
